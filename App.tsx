7131010811aaf6fd220f7ca4318407a13c7be952
Shivani Upadhyay <68890988+in09shivani@users.noreply.github.com>
2025-09-18 00:56:16 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-brand-dark font-sans">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
